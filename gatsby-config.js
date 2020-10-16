15ba626fe6cdf784be3ddc55332526fbf075197f
bottlehs <qudgns9@gmail.com>
2020-10-16 18:30:24 +0900

    google-tagmanager

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,6 +79,28 @@ module.exports = {
         enableIdentityWidget: false,
       },
     },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-5ZCPJS2",
+
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+  
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+  
+        // Specify optional GTM environment details.
+        /*
+        gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        dataLayerName: "YOUR_DATA_LAYER_NAME",
+        */
+      },
+    },    
     `gatsby-plugin-offline`,
     `gatsby-plugin-feed`,    
     `gatsby-plugin-react-helmet`,
