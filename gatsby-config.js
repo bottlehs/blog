cb00b52f32d22c047a436bf6bc67c3f2951001ac
bottlehs <qudgns9@gmail.com>
2020-11-06 01:53:05 +0900

    ga 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,28 +79,6 @@ module.exports = {
         enableIdentityWidget: false,
       },
     },
-    {
-      resolve: "gatsby-plugin-google-tagmanager",
-      options: {
-        id: "G-0H99L5VC01",
-
-        // Include GTM in development.
-        // Defaults to false meaning GTM will only be loaded in production.
-        includeInDevelopment: false,
-  
-        // datalayer to be set before GTM is loaded
-        // should be an object or a function that is executed in the browser
-        // Defaults to null
-        defaultDataLayer: { platform: "gatsby" },
-  
-        // Specify optional GTM environment details.
-        /*
-        gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
-        gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
-        dataLayerName: "YOUR_DATA_LAYER_NAME",
-        */
-      },
-    },    
     `gatsby-plugin-offline`,
     `gatsby-plugin-feed`,    
     `gatsby-plugin-react-helmet`,
