9cdd16d3c9f4f77cf3d4ecf09e9d5a24969cfba7
bottlehs <qudgns9@gmail.com>
2020-11-12 22:05:14 +0900

    a title

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,12 +9,12 @@ const Layout = ({ location, title, children }) => {
   if (isRootPath) {
     header = (
       <h1 className="main-heading">
-        <Link to="/">{title}</Link>
+        <Link to="/" title={title}>{title}</Link>
       </h1>
     )
   } else {
     header = (
-      <Link className="header-link-home" to="/">
+      <Link className="header-link-home" to="/" title={title}>
         {title}
       </Link>
     )
