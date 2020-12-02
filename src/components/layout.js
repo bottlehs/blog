a124d4d9b48ff1e3e341cb8805ddd5d1a84c771c
bottlehs <qudgns9@gmail.com>
2020-12-03 05:08:41 +0900

    dark mode

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,8 @@ import React from "react"
 import { Link } from "gatsby"
 import { ThemeToggler } from 'gatsby-plugin-dark-mode'
 import Switch from "react-switch";
-import { RiSunLine, RiMoonClearLine } from 'react-icons/ri';
+import { IconContext } from "react-icons";
+import { RiSunFill, RiMoonClearFill } from 'react-icons/ri';
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
@@ -25,26 +26,34 @@ const Layout = ({ location, title, children }) => {
 
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
+      <div className="theme">
+        <ThemeToggler>
+          {({ theme, toggleTheme }) => (
+            <Switch 
+              onChange={e => toggleTheme(theme === 'dark' ? 'light' : 'dark')} 
+              checked={theme === 'dark'}
+              offColor="#b2dcf2"
+              onColor="#423090"
+              uncheckedIcon={
+                <IconContext.Provider value={{ color: "#f4da87", className: "theme-sun-icon" }}>
+                  <RiSunFill />
+                </IconContext.Provider>
+              }
+              checkedIcon={
+                <IconContext.Provider value={{ color: "#e7eee5", className: "theme-moon-icon" }}>
+                  <RiMoonClearFill  />
+                </IconContext.Provider>
+              }
+              />
+          )}
+        </ThemeToggler>        
+      </div> 
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
         © {new Date().getFullYear()}, Built with
         {` `}
-        <a href="https://www.bottlehs.com">Bottlehs</a>
-        <ThemeToggler>
-        {({ theme, toggleTheme }) => (
-          <Switch 
-            onChange={e => toggleTheme(theme === 'dark' ? 'light' : 'dark')} 
-            checked={theme === 'dark'}
-            uncheckedIcon={
-              <RiSunLine />
-            }
-            checkedIcon={
-              <RiMoonClearLine />
-            }
-            />
-        )}
-      </ThemeToggler>        
+        <a href="https://www.bottlehs.com">Bottlehs</a>     
       </footer>
     </div>
   )
