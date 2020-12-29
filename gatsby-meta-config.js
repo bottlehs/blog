0cb376082969bb081cf41f16426411282404f645
bottlehs <qudgns9@gmail.com>
2020-12-29 23:41:29 +0900

    메타 태그 수정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: `Bottlehs Tech Blog`,
   author: {
     name: `Jeon Byung Hun`,
-    summary: `개발을 즐기는 bottlehs - Engineer, MS, AI, FE, BE, OS, IOT, 설계, 테스트`,
+    summary: `개발을 즐기는 bottlehs - Engineer, MS, AI, FE, BE, OS, IOT, Blockchain, 설계, 테스트`,
   },
   description: `다양한 IT 기술 및 프로그래밍 관련 글을 작성한다`,
   siteUrl: `https://bottlehs.com`,
@@ -11,6 +11,6 @@ module.exports = {
   },
 
   icon: `content/assets/bottlehs-icon.png`,
-  keywords: [`blog`,`it`,`javascript`,`php`,`frontend`,`backend`,`ai`,`os`,`설계`,`테스트`,`java`,`tech blog`],
+  keywords: [`blog`,`it`,`javascript`,`php`,`frontend`,`backend`,`ai`,`os`,`설계`,`테스트`,`java`,`Blockchain`,`tech blog`],
   googleAnalyticsId: 'UA-180287321-1',
 }
