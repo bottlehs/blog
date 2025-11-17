945e8f7512c180688bc2498a990daea2f22763dc
nextinnovation-huns <qudgns9@gmail.com>
2025-11-18 03:53:48 +0900

    style

--- src/components/tableOfContents.js
+++ src/components/tableOfContents.js
@@ -1,7 +1,10 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useEffect, useRef } from 'react';
 import cn from 'classnames';
 
 const TableOfContents = ({ items, currentHeaderUrl }) => {
+  const navRef = useRef(null);
+  const contentsRef = useRef(null);
+
   const replaceItems = useMemo(() => {
     if (currentHeaderUrl) {
       return items.replace(
@@ -12,10 +15,32 @@ const TableOfContents = ({ items, currentHeaderUrl }) => {
       return items;
     }
   }, [currentHeaderUrl]);
+
+  useEffect(() => {
+    if (currentHeaderUrl && navRef.current && contentsRef.current) {
+      const currentLink = contentsRef.current.querySelector('a.isCurrent');
+      if (currentLink) {
+        const nav = navRef.current;
+        const linkRect = currentLink.getBoundingClientRect();
+        const navRect = nav.getBoundingClientRect();
+        const linkTop = linkRect.top - navRect.top + nav.scrollTop;
+        const navHeight = nav.offsetHeight;
+        const linkHeight = linkRect.height;
+        const targetScrollTop = linkTop - (navHeight / 2) + (linkHeight / 2);
+
+        nav.scrollTo({
+          top: targetScrollTop,
+          behavior: 'smooth'
+        });
+      }
+    }
+  }, [currentHeaderUrl]);
+
   return items ? (
-    <nav className={cn('table-of-contents', 'container')}>
+    <nav ref={navRef} className={cn('table-of-contents', 'container')}>
       <h3 className={'title'}>Table of contents</h3>
       <div
+        ref={contentsRef}
         className={'contents'}
         dangerouslySetInnerHTML={{ __html: replaceItems }}
       />
